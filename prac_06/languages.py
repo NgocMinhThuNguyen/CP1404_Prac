e0bc00ddc20f662166a52a2c459a49dec02e54d6
NgocMinhThuNguyen <ngocminhthu.nguyen@my.jcu.edu.au>
2021-12-28 17:37:31 +0700

    Update languages exercise

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,6 +2,7 @@ from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
+    """Programming languages program"""
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
