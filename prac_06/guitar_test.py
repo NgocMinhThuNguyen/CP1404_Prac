762437fda12bcf1c149a9d1a430349581db2c47a
NgocMinhThuNguyen <ngocminhthu.nguyen@my.jcu.edu.au>
2021-12-28 17:18:34 +0700

    Update guitar test exercise

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -2,7 +2,7 @@ from prac_06.guitar import Guitar
 
 
 def main():
-
+    """Test guitar program"""
     guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
     other_guitar = Guitar("Another Guitar", 2012, 1512.9)
 
