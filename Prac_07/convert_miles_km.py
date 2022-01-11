0613257f7f56073dede7e22d11813225032c320c
NgocMinhThuNguyen <ngocminhthu.nguyen@my.jcu.edu.au>
2022-01-11 16:24:52 +0700

    Update taxi test exercise

--- Prac_07/convert_miles_km.py
+++ Prac_07/convert_miles_km.py
@@ -1,34 +1,35 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+
 MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
-    """ MilesConverterApp  for converting m to km """
+    """Miles Converter App"""
     def build(self):
-        """ build the Kivy app from the kv file """
+        """Build the Kivy app from the kv file """
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def convert(self):
         """Convert m to km"""
-        value = self.get_validated_miles()
-        result = value * MILES_TO_KM
+        user_input = self.get_validated_miles()
+        result = user_input * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, change):
         """handle up/down button press"""
-        value = self.get_validated_miles() + change
-        self.root.ids.input_miles.text = str(value)
+        user_input = self.get_validated_miles()
+        self.root.ids.input_miles.text = str(user_input + change)
         self.convert()
 
     def get_validated_miles(self):
         """get valid input from user"""
         try:
-            value = float(self.root.ids.input_miles.text)
-            return value
+            user_input = float(self.root.ids.input_miles.text)
+            return user_input
         except ValueError:
             return 0
 
--- Prac_07/squaring.py
+++ Prac_07/squaring.py
@@ -4,16 +4,16 @@ from kivy.core.window import Window
 
 
 class SquareNumberApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
+    """ Square Number App"""
     def build(self):
-        """ build the Kivy app from the kv file """
+        """Build the Kivy app from the kv file """
         Window.size = (200, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
+        """Handle calculation (could be button press or other call), output result to label widget """
         result = value ** 2
         self.root.ids.output_label.text = str(result)
 
