58ea2ab9833ac58f1b498aa798cba3b77771484f
NgocMinhThuNguyen <ngocminhthu.nguyen@my.jcu.edu.au>
2022-01-18 18:59:14 +0700

    Update clean up files exercise

--- Prac_09/cleanup_files.py
+++ Prac_09/cleanup_files.py
@@ -2,23 +2,38 @@ import os
 
 
 def main():
+    """Clean up program"""
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-
         for filename in filenames:
             old_name = os.path.join(directory_name, filename)
             new_name = os.path.join(directory_name, get_fixed_filename(filename))
             os.rename(old_name, new_name)
+            print(new_name)
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = ""
+    old_name = (filename.replace('.TXT', '.txt').replace('.txt', ''))
+
+    for i, letter in enumerate(old_name):
+        if letter.isspace():
+            letter = "_"
+        elif letter.isalpha():
+            try:
+                previous_char = old_name[i - 1]
+                next_letter = old_name[i + 1]
+                if next_letter.isupper() or next_letter == "(":
+                    letter += "_"
+                elif previous_char == "_":
+                    letter = letter.upper()
+            except IndexError:
+                pass
+
+        new_name += letter
+    new_name += '.txt'
     return new_name
 
 
-# main()
+main()
